0b77980ee70e9287d7947ef84e7aa1e046cf227e
Tushar Choudhary <chaudharyt99@gmail.com>
2025-11-16 02:56:57 -0800

    added machine learning bg

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,13 +4,6 @@ export const Home = () => {
   return (
     <div className='relative min-h-screen text-foreground overflow-hidden'>
       <DecorativeBg />
-      <main className='relative z-10 flex min-h-screen items-center justify-center'>
-        {/* Background Effects */}
-        {/* <StarBackground /> */}
-        {/* Navbar */}
-        {/* Main content */}
-        {/* Footer */}
-      </main>
     </div>
   )
 }
